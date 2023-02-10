b7f618ed9330e900d39070ed294da5d320c8f8ac
Arjun Biju <arjun7.biju@gmail.com>
2023-02-10 21:00:18 +0530

    AJAX checkbox not working

---
+++ todo.js
@@ -0,0 +1,27 @@
+let xhttp = new XMLHttpRequest;
+xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+xhttp.send();
+xhttp.onreadystatechange= function(){
+    if(this.readyState==4 && this.status == 200){
+        var output = JSON.parse(this.responseText);
+        var arrayy=""
+        for(let i=0;i<output.length;++i){
+            arrayy += "<tr> <td>" + output[i].id + "</td>"+"<td>" + output[i].title + "</td>";
+            if(output[i].completed==true){
+                let elements = document.createElement("input");
+                elements.type = 'checkbox';
+                elements.setAttribute("id",`cb${i}`)
+                arrayy += "<td>" + elements + "</td> </tr>";
+                console.log(elements.id)
+            }
+            else{
+                let elementz = document.createElement("input");
+                elementz.type = 'checkbox';
+                elementz.setAttribute("id",`cb${i}`)
+                arrayy += "<td>" + elementz + "</td> </tr>";
+                console.log(elementz.id);
+            }
+        }
+        document.getElementById("todo").innerHTML=arrayy;
+    }
+}
